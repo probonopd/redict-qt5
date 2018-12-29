a714cd6a05fdb9c6d47d88b6c9057430e2e8a7cc
rekols <unsonwong@gmail.com>
2018-12-30 01:41:33 +0800

    updage theme color

--- src/sidebutton.cpp
+++ src/sidebutton.cpp
@@ -5,23 +5,6 @@ SideButton::SideButton(QWidget *parent)
     : QPushButton(parent)
 {
     setObjectName("SideButton");
-    setStyleSheet("#SideButton {"
-                  "margin: 0;"
-                  "text-align: left;"
-                  "padding: 5px 15px;"
-                  "border: none;"
-                  "}"
-                  "#SideButton:hover {"
-                  "background-color: rgb(237, 237, 237);"
-                  "}"
-                  "#SideButton:checked {"
-                  "color: rgba(50, 115, 220);"
-                  "font-weight: 500;"
-                  "background-color: rgba(50, 115, 220, 0.1);"
-                  "border: 1px solid rgba(50, 115, 220, 0.1);"
-                  "border-left: none;"
-                  "border-right: 5px;"
-                  "}");
 }
 
 void SideButton::paintEvent(QPaintEvent *e)
@@ -38,5 +21,5 @@ void SideButton::paintEvent(QPaintEvent *e)
     QRect sep_path = rect();
     sep_path.setLeft(0);
     sep_path.setWidth(4);
-    painter.fillRect(sep_path, QColor(50, 115, 220));
+    painter.fillRect(sep_path, QColor(66, 133, 244));
 }
