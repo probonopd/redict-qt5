74f2e6810f9db3ccf1a61a9ab2ce16aeb0ca5eb5
rekols <unsonwong@gmail.com>
2019-01-01 03:37:28 +0800

    add build_appimage.sh

---
+++ build_appimage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+
+qmake CONFIG+=release PREFIX=/usr
+make -j$(nproc)
+mkdir -p appdir/usr/bin/ appdir/usr/share/applications/ appdir/usr/share/applications/ appdir/usr/share/icons/hicolor/scalable/apps
+cp redict appdir/usr/bin/
+cp redict.desktop appdir/usr/share/applications/
+cp images/redict.svg appdir/usr/share/icons/hicolor/scalable/apps/redict.svg
+find appdir/
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
