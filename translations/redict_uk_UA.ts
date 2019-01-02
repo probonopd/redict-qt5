e232c5e2ca01484fe67de164f03024f6608d77a2
app4soft <appsoft@ua.fm>
2019-01-02 13:43:16 +0200

    Create redict_uk_UA.ts

---
+++ translations/redict_uk_UA.ts
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../src/aboutpage.cpp" line="22"/>
+        <source>Version</source>
+        <translation>Версія</translation>
+    </message>
+</context>
+<context>
+    <name>DictPage</name>
+    <message>
+        <location filename="../src/dictpage.cpp" line="93"/>
+        <source>UK</source>
+        <translation>UK</translation>
+    </message>
+    <message>
+        <location filename="../src/dictpage.cpp" line="102"/>
+        <source>US</source>
+        <translation>US</translation>
+    </message>
+</context>
+<context>
+    <name>DonatePage</name>
+    <message>
+        <location filename="../src/donatepage.cpp" line="12"/>
+        <source>Welcome to donate to me</source>
+        <translation>Буду вдячний за пожертви</translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <location filename="../src/homepage.cpp" line="18"/>
+        <source>Enter the word or phrase you want to query</source>
+        <translation>Введіть слово чи фразу яку ви хочете перекласти</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressPage</name>
+    <message>
+        <location filename="../src/progresspage.cpp" line="19"/>
+        <source>Unable to access the network.</source>
+        <translation>Відсутній доступ до мережі.</translation>
+    </message>
+    <message>
+        <location filename="../src/progresspage.cpp" line="20"/>
+        <source>Reload</source>
+        <translation>Перезавантажити</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../src/settingspage.cpp" line="25"/>
+        <source>Turn on the system tray icon</source>
+        <translation>Увімкнути іконку в системному лотку</translation>
+    </message>
+</context>
+<context>
+    <name>SideBar</name>
+    <message>
+        <location filename="../src/sidebar.cpp" line="13"/>
+        <source>Home</source>
+        <translation>Головна</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="13"/>
+        <source>Translation</source>
+        <translation>Переклад</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="15"/>
+        <source>About</source>
+        <translation>Про</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="14"/>
+        <source>Donate</source>
+        <translation>Пожертвування</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="14"/>
+        <source>Setttings</source>
+        <translation>Налаштування</translation>
+    </message>
+</context>
+<context>
+    <name>TranslatePage</name>
+    <message>
+        <location filename="../src/translatepage.cpp" line="21"/>
+        <source>Translation</source>
+        <translation>Переклад</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="22"/>
+        <source>Automatic detection</source>
+        <translation>Автоматичне визначення</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="23"/>
+        <source>Chinese to English</source>
+        <translation>З китайської на англійську</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="24"/>
+        <source>Chinese to Japanese</source>
+        <translation>З китайської на японську</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="25"/>
+        <source>Chinese to Korean</source>
+        <translation>З китайської на корейську</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="26"/>
+        <source>Chinese to French</source>
+        <translation>З китайської на французьку</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="27"/>
+        <source>Chinese to Russian</source>
+        <translation>З китайсьокї на російську</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="28"/>
+        <source>Chinese to Spanish</source>
+        <translation>З китайської на іспанську</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="29"/>
+        <source>English to Chinese</source>
+        <translation>З англійської на китайську</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="45"/>
+        <source>Please enter the text you want to translate</source>
+        <translation>Будь-ласка, введіть текст котрий потрібно перекласти</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../src/widgets/trayicon.cpp" line="6"/>
+        <source>Open</source>
+        <translation>Відкрити</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/trayicon.cpp" line="7"/>
+        <source>Exit</source>
+        <translation>Вийти</translation>
+    </message>
+</context>
+</TS>
