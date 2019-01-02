2098bbe065e95631751e75562774aa599b3573ce
probonopd <probonopd@users.noreply.github.com>
2019-01-02 03:08:32 +0000

    Create redict.de_DE.ts

---
+++ translations/redict.de_DE.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../src/aboutpage.cpp" line="22"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+</context>
+<context>
+    <name>DictPage</name>
+    <message>
+        <location filename="../src/dictpage.cpp" line="103"/>
+        <source>UK</source>
+        <translation>UK</translation>
+    </message>
+    <message>
+        <location filename="../src/dictpage.cpp" line="112"/>
+        <source>US</source>
+        <translation>US</translation>
+    </message>
+</context>
+<context>
+    <name>DonatePage</name>
+    <message>
+        <location filename="../src/donatepage.cpp" line="12"/>
+        <source>Welcome to donate to me</source>
+        <translation>Danke für Spenden an mich</translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <location filename="../src/homepage.cpp" line="18"/>
+        <source>Enter the word or phrase you want to query</source>
+        <translation>Zu übersetzendes Wort oder Satz eingeben</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressPage</name>
+    <message>
+        <location filename="../src/progresspage.cpp" line="19"/>
+        <source>Unable to access the network.</source>
+        <translation>Kann nicht auf das Netzwerk zugreifen.</translation>
+    </message>
+    <message>
+        <location filename="../src/progresspage.cpp" line="20"/>
+        <source>Reload</source>
+        <translation>Neu laden</translation>
+    </message>
+</context>
+<context>
+    <name>SideBar</name>
+    <message>
+        <location filename="../src/sidebar.cpp" line="13"/>
+        <source>Home</source>
+        <translation>Startseite</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="13"/>
+        <source>Translation</source>
+        <translation>Übersetzung</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="13"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="13"/>
+        <source>Donate</source>
+        <translation>Spenden</translation>
+    </message>
+</context>
+<context>
+    <name>TranslatePage</name>
+    <message>
+        <location filename="../src/translatepage.cpp" line="21"/>
+        <source>Translation</source>
+        <translation>Übersetzung</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="22"/>
+        <source>Automatic detection</source>
+        <translation>Automatische Erkennung</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="23"/>
+        <source>Chinese to English</source>
+        <translation>Chinesisch zu Englisch</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="24"/>
+        <source>Chinese to Japanese</source>
+        <translation>Chinesisch zu Japanisch</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="25"/>
+        <source>Chinese to Korean</source>
+        <translation>Chinesisch zu Koreanisch</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="26"/>
+        <source>Chinese to French</source>
+        <translation>Chinesisch zu Französisch</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="27"/>
+        <source>Chinese to Russian</source>
+        <translation>Chinesisch zu Russisch</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="28"/>
+        <source>Chinese to Spanish</source>
+        <translation>Chinesisch zu Spanisch</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="29"/>
+        <source>English to Chinese</source>
+        <translation>Englisch zu Chinesisch</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="45"/>
+        <source>Please enter the text you want to translate</source>
+        <translation>Bitte geben Sie den zu übersetzenden Text ein</translation>
+    </message>
+</context>
+</TS>
