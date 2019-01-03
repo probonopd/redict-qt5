10fd37759e716bcc0e4cb0a4a556ef5366ea67a4
Nikolay Korotkiy <sikmir@gmail.com>
2019-01-04 01:19:49 +0300

    Add Russian translation

---
+++ translations/redict_ru_RU.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../src/aboutpage.cpp" line="22"/>
+        <source>Version</source>
+        <translation>Версия</translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <location filename="../src/homepage.cpp" line="18"/>
+        <source>Enter the word or phrase you want to query</source>
+        <translation>Введите слово или фразу для запроса</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressPage</name>
+    <message>
+        <location filename="../src/progresspage.cpp" line="19"/>
+        <source>Unable to access the network.</source>
+        <translation>Не удается получить доступ к сети.</translation>
+    </message>
+    <message>
+        <location filename="../src/progresspage.cpp" line="20"/>
+        <source>Reload</source>
+        <translation>Перезагрузить</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../src/settingspage.cpp" line="25"/>
+        <source>Turn on the system tray icon</source>
+        <translation>Включить значок в системном трее</translation>
+    </message>
+</context>
+<context>
+    <name>SideBar</name>
+    <message>
+        <location filename="../src/sidebar.cpp" line="14"/>
+        <source>Home</source>
+        <translation>Главная</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="14"/>
+        <source>Translation</source>
+        <translation>Перевод</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="15"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="18"/>
+        <source>Donate</source>
+        <translation>Пожертвования</translation>
+    </message>
+    <message>
+        <location filename="../src/sidebar.cpp" line="15"/>
+        <source>Setttings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>TranslatePage</name>
+    <message>
+        <location filename="../src/translatepage.cpp" line="21"/>
+        <source>Translation</source>
+        <translation>Перевести</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="22"/>
+        <source>Automatic detection</source>
+        <translation>Автоматическое определение</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="23"/>
+        <source>Chinese to English</source>
+        <translation>С китайского на английский</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="24"/>
+        <source>Chinese to Japanese</source>
+        <translation>С китайского на японский</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="25"/>
+        <source>Chinese to Korean</source>
+        <translation>С китайского на корейский</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="26"/>
+        <source>Chinese to French</source>
+        <translation>С китайского на французский</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="27"/>
+        <source>Chinese to Russian</source>
+        <translation>С китайского на русский</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="28"/>
+        <source>Chinese to Spanish</source>
+        <translation>С китайского на испанский</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="29"/>
+        <source>English to Chinese</source>
+        <translation>С английского на китайский</translation>
+    </message>
+    <message>
+        <location filename="../src/translatepage.cpp" line="45"/>
+        <source>Please enter the text you want to translate</source>
+        <translation>Пожалуйста, введите текст для перевода</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../src/widgets/trayicon.cpp" line="6"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/trayicon.cpp" line="7"/>
+        <source>Exit</source>
+        <translation>Выйти</translation>
+    </message>
+</context>
+<context>
+    <name>YoudaoDictWidget</name>
+    <message>
+        <location filename="../src/widgets/youdaodictwidget.cpp" line="78"/>
+        <source>UK</source>
+        <translation>UK</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/youdaodictwidget.cpp" line="87"/>
+        <source>US</source>
+        <translation>US</translation>
+    </message>
+</context>
+</TS>
